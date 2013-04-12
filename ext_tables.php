a8da1245d81a96e29abf792aec0640730284beb4
Thorsten Boock <thorsten@nerdcenter.de>
2013-04-12 15:54:47 +0200

    Replaced t3lib_extMgm with \TYPO3\CMS\Core\Utility\ExtensionManagementUtility

--- ext_tables.php
+++ ext_tables.php
@@ -4,4 +4,4 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 't3b_config');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 't3b_config');
