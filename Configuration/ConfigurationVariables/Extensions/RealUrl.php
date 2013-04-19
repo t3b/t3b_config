6f3d86fa036d1898e315e160b958290753e8ee42
Thorsten Boock <thorsten@nerdcenter.de>
2013-04-19 14:43:05 +0200

    RealURL prevars: set English as the default language

--- Configuration/ConfigurationVariables/Extensions/RealUrl.php
+++ Configuration/ConfigurationVariables/Extensions/RealUrl.php
@@ -6,79 +6,79 @@ if (!defined('TYPO3_MODE')) {
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl'] = array(
 	'_DEFAULT' => array(
-	'init' => array(
-		'enableCHashCache' => '1',
-		'appendMissingSlash' => 'ifNotFile',
-		'enableUrlDecodeCache' => '1',
-		'enableUrlEncodeCache' => '1',
-	),
-	'redirects' => array(
-	),
-	'preVars' => array(
-		'0' => array(
-		'GETvar' => 'no_cache',
-		'valueMap' => array(
-			'nc' => '1',
+		'init' => array(
+			'enableCHashCache' => '1',
+			'appendMissingSlash' => 'ifNotFile',
+			'enableUrlDecodeCache' => '1',
+			'enableUrlEncodeCache' => '1',
 		),
-		'noMatch' => 'bypass'
+		'redirects' => array(
 		),
-		'1' => array(
-		'GETvar' => 'L',
-		'valueMap' => array(
-			'de' => '0',
-			'en' => '1',
-			'fr' => '2',
-		),
-		'noMatch' => 'bypass',
-		),
-		'2' => array(
-		'GETvar' => 'lang',
-		'valueMap' => array(
-			'de' => 'de',
-			'en' => 'en',
-			'fr' => 'fr',
-		),
-		'noMatch' => 'bypass',
-		),
-	),
-	'pagePath' => array(
-		'type' => 'user',
-		'userFunc' => 'EXT:realurl/class.tx_realurl_advanced.php:&tx_realurl_advanced->main',
-		'spaceCharacter' => '-',
-		'languageGetVar' => 'L',
-		'expireDays' => '7',
-		'rootpage_id' => '1',
-	),
-	'fixedPostVars' => array(
-	),
-	'postVarSets' => array(
-		'_DEFAULT' => array(
-		),
-	),
-	'fileName' => array(
-		'defaultToHTMLsuffixOnPrev' => FALSE,
-		'index' => array(
-		'rss.xml' => array(
-			'keyValues' => array(
-			'type' => '100',
+		'preVars' => array(
+			'0' => array(
+				'GETvar' => 'no_cache',
+				'valueMap' => array(
+					'nc' => '1',
+				),
+				'noMatch' => 'bypass'
+			),
+			'1' => array(
+				'GETvar' => 'L',
+				'valueMap' => array(
+					'en' => '0',
+					'de' => '1',
+					'fr' => '2',
+				),
+				'noMatch' => 'bypass',
+			),
+			'2' => array(
+				'GETvar' => 'lang',
+				'valueMap' => array(
+					'en' => 'en',
+					'de' => 'de',
+					'fr' => 'fr',
+				),
+				'noMatch' => 'bypass',
 			),
 		),
-		'rss091.xml' => array(
-			'keyValues' => array(
-			'type' => '101',
+		'pagePath' => array(
+			'type' => 'user',
+			'userFunc' => 'EXT:realurl/class.tx_realurl_advanced.php:&tx_realurl_advanced->main',
+			'spaceCharacter' => '-',
+			'languageGetVar' => 'L',
+			'expireDays' => '7',
+			'rootpage_id' => '1',
+		),
+		'fixedPostVars' => array(
+		),
+		'postVarSets' => array(
+			'_DEFAULT' => array(
 			),
 		),
-		'rdf.xml' => array(
-			'keyValues' => array(
-			'type' => '102',
+		'fileName' => array(
+			'defaultToHTMLsuffixOnPrev' => FALSE,
+			'index' => array(
+				'rss.xml' => array(
+					'keyValues' => array(
+						'type' => '100',
+					),
+				),
+				'rss091.xml' => array(
+					'keyValues' => array(
+						'type' => '101',
+					),
+				),
+				'rdf.xml' => array(
+					'keyValues' => array(
+						'type' => '102',
+					),
+				),
+				'atom.xml' => array(
+					'keyValues' => array(
+						'type' => '103',
+					),
+				),
 			),
 		),
-		'atom.xml' => array(
-			'keyValues' => array(
-			'type' => '103',
-			),
-		),
-		),
-	),
 	),
 );
