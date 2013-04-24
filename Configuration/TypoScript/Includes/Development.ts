3f4bec489dfc4415bf8a9d58a2e922337a4015c5
Inkdpixels <inkdpixels@googlemail.com>
2013-04-24 12:54:39 +0200

    Add a missing z-index to the "Development"-Banner

--- Configuration/TypoScript/Includes/Development.ts
+++ Configuration/TypoScript/Includes/Development.ts
@@ -20,4 +20,4 @@ config {
 
 # display a small development environment notice
 page.999999 = TEXT
-page.999999.value = <div style="position:fixed;top:0;right:0;padding:4px;background:#ff4d00;color:#fff;font:bold 11px verdana,sans;text-shadow:1px 1px 1px #401300;">Development</div>
+page.999999.value = <div style="position:fixed;top:0;right:0;z-index:99999;padding:4px;background:#ff4d00;color:#fff;font:bold 11px verdana,sans;text-shadow:1px 1px 1px #401300;">Development</div>
