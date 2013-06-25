9aa5149fcf3625d5a19db5a302b3c34ff37f8bd9
Tyll Weiß <inkdpixels@googlemail.com>
2013-06-25 11:00:10 +0200

    Remove the meta-viewport tag by default
    This is an optional meta tag and should not be set by default for any project, this prevents an unwanted zoomed in view on page load for mobile users.

--- Configuration/TypoScript/Includes/Page.ts
+++ Configuration/TypoScript/Includes/Page.ts
@@ -36,9 +36,6 @@ page {
 			data = page:author
 			ifEmpty.data = levelfield:0, author
 		}
-
-		# tell mobile devices to initially scale the page to fit the devices screen width
-		viewport = width=device-width, initial-scale=1.0
 	}
 
 	shortcutIcon = {$plugin.t3b_config.settings.page.shortcutIcon}
