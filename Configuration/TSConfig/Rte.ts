bbd3263b983a3777d969023da3dc970de5052399
Thorsten Boock <thorsten@nerdcenter.de>
2013-07-01 09:46:59 +0200

    [FEATURE] Added RTE configuration with the goal of providing a collection of often used styles. For now, this only consists of a "zebra" configuration for table / tr / td / thead -tags.

---
+++ Configuration/TSConfig/Rte.ts
@@ -0,0 +1,35 @@
+# A collection of often used styles. This file is not included by default. To load it inside your own template extension, use the following typoscript:
+# <INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3b_config/Configuration/TSConfig/Rte.ts">
+# RTE.default.contentCSS = EXT:my_template_extension/Resources/Public/Stylesheets/Rte.css
+
+RTE {
+	classes.zebra {
+		name = Zebra
+		alternating {
+			rows {
+				startAt = 1
+				oddClass = tr-odd
+				evenClass = tr-even
+				oddHeaderClass = thead-odd
+				evenHeaderClass = thead-even
+			}
+			columns {
+				startAt = 1
+				oddClass = td-odd
+				evenClass = td-even
+				oddHeaderClass = th-odd
+				evenHeaderClass = th-even
+            }
+		}
+	}
+	default {
+		proc.allowedClasses := addToList(zebra, tr-even, tr-odd, th-odd, th-even, td-odd, td-even)
+		buttons.blockstyle.tags {
+			table.allowedClasses := addToList(zebra)
+			tr.allowedClasses := addToList(tr-odd, tr-even, thead-odd, thead-even)
+			th.allowedClasses := addToList(th-odd, th-even)
+			td.allowedClasses := addToList(td-odd, td-even, thead-odd, thead-even)
+		}
+	}
+	mutuallyExclusiveClasses = tr-odd, tr-even, th-odd, th-even, td-odd, td-even
+}
