543be7433284de9a00b0dcd2c887d8ee2f2b237b
Thorsten Boock <thorsten@nerdcenter.de>
2013-07-22 15:31:45 +0200

    [TASK] Updated configuration on how to use the zebra-table-class.

--- Configuration/TSConfig/Rte.ts
+++ Configuration/TSConfig/Rte.ts
@@ -1,6 +1,9 @@
-# A collection of often used styles. This file is not included by default. To load it inside your own template extension, use the following typoscript:
+# A collection of often used styles. This file is not included by default. To load it inside your own template extension, use the following page typoscript:
 # <INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3b_config/Configuration/TSConfig/Rte.ts">
 # RTE.default.contentCSS = EXT:my_template_extension/Resources/Public/Stylesheets/Rte.css
+#
+# If you want to use the zebra-table-class, you'll need the following frontend typoscript configuration:
+# lib.parseFunc_RTE.externalBlocks.table.stdWrap.HTMLparser.tags.table.fixAttrib.class.list >
 
 RTE {
 	classes.zebra {
